1e168b0660d011a8add413cc0816ab567ad15f6f
torbatti <torbattii@gmail.com>
2025-05-28 15:53:49 +0330

    reworking view component

--- src/App.tsx
+++ src/App.tsx
@@ -1,64 +1,97 @@
-import { createEffect, createSignal } from "solid-js";
+import { Accessor, createEffect, createSignal, Setter } from "solid-js";
+import { Switch, Match } from "solid-js"
 
 import "./styles/albors.css";
 import "./styles/root.css";
 
 import "./styles/view.css";
-import "./styles/v-bala.css";
-import "./styles/v-chap.css";
-import "./styles/v-mian.css";
-import "./styles/v-rast.css";
-import "./styles/v-fari.css";
 
-type View = "asli" | "fari"
+
+type ViewStruct = {
+  left: {gt:Accessor<string>,st:Setter<string>}
+  top: {gt:Accessor<string>,st:Setter<string>}
+  main: {gt:Accessor<string>,st:Setter<string>}
+  mleft: {gt:Accessor<string>,st:Setter<string>}
+  mright: {gt:Accessor<string>,st:Setter<string>}
+}
 
 function App() {
-  const [view,setView] = createSignal("fari")
-  
-  const [viewAsli,setViewAsli] = createSignal("")
-  const [viewChap,setViewChap] = createSignal("")
-  const [viewMian,setViewMian] = createSignal("")
-  const [viewRast,setViewRast] = createSignal("")
-  const [viewBala,setViewBala] = createSignal("")
-  
-  const [viewFari,setViewFari] = createSignal("")
+  const [view, setView] = createSignal("")
 
+  const [viewLeft, setViewLeft] = createSignal("")
+  const [viewTop, setViewTop] = createSignal("")
+  const [viewMain, setViewMain] = createSignal("")
+  const [viewMainLeft, setViewMainLeft] = createSignal("")
+  const [viewMainRight, setViewMainRight] = createSignal("")
+
+  const view_struct: ViewStruct = {
+    left:{gt: viewLeft, st:setViewLeft},
+    top:{gt: viewTop, st:setViewTop},
+    main:{gt: viewMain, st:setViewMain},
+    mleft:{gt: viewMainLeft, st:setViewMainLeft},
+    mright:{gt: viewMainRight, st:setViewMainRight},
+  }
 
   return (
     <div id="view">
-
-      <div id="v-chap-pooste">
-        <div id="v-chap-haste">
-
-        </div>
-      </div>
-
-      <div id="v-bala-pooste">
-        <div id="v-bala-haste">
-
-        </div>
-      </div>
-
-      <div id="v-mian-pooste">
-        <div id="v-mian-haste">
-
-        </div>
-      </div>
-
-      <div id="v-rast-pooste">
-        <div id="v-rast-haste">
-
-        </div>
-      </div>
-
-      <div id="v-fari-pooste">
-        <div id="v-fari-haste">
-
-        </div>
-      </div>
-
+      <ViewBar />
+      <ViewTop />
+      <ViewMain/>
     </div>
   );
+}
+
+function ViewBar() {
+  return (
+    <div id="view-bar">
+
+    </div>
+  )
+}
+
+function ViewTop() {
+  return (
+    <div id="view-top">
+
+    </div>
+  )
+}
+
+function ViewMain() {
+  return (
+    <div id="view-main">
+
+      <Switch>
+        <Match when={condition1}>
+          <ViewMainLeft/>
+        </Match>
+        <Match when={condition2}>
+          <ViewMainRight/>
+        </Match>
+      </Switch>
+
+    </div>
+  )
+}
+function ViewMainLeft() {
+  return (
+    <div id="view-main-left">
+    </div>
+  )
+}
+
+function ViewMainRight() {
+  return (
+    <div id="view-main-right">
+    </div>
+  )
+}
+
+function ViewMainPage() {
+  return (
+    <div id="view-main-page">
+    </div>
+  )
 }
 
 export default App;
